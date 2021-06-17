b40bf4022d6265b7a70a49f0bd443067e659b2c0
zjamali <zouhairjamali1998@gmail.com>
2021-06-17 15:36:57 +0100

    parse aguments done

--- main.c
+++ main.c
@@ -6,23 +6,54 @@
 /*   By: zjamali <zjamali@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/16 20:21:59 by zjamali           #+#    #+#             */
-/*   Updated: 2021/06/16 21:54:12 by zjamali          ###   ########.fr       */
+/*   Updated: 2021/06/17 15:28:47 by zjamali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-// memcpy(v.items, &(int[1]){5}, 4);
+#include <stdio.h>
 
 void	ft_init_stacks(t_stack *stacks)
 {
-	ft_vector_init(&stacks->a,sizeof(int));
-	ft_vector_init(&stacks->b,sizeof(int));
+	ft_vector_init(&stacks->a, sizeof(int));
+	ft_vector_init(&stacks->b, sizeof(int));
+}
+
+char	**parse_arguments(char **av, int ac)
+{
+	char	**data;
+	char	*line;
+	int		i;
+	char	*tmp;
+
+	i = 1;
+	line = ft_strdup("");
+	data = NULL;
+	while (i < ac)
+	{
+		tmp = line;
+		line = ft_strjoin(line, av[i]);
+		free(tmp);
+		tmp = line;
+		line = ft_strjoin(line," ");
+		free(tmp);
+		i++;
+	}
+	data = ft_split(line,' ');
+	free(line);
+	return (data);
 }
 
 int	main(int ac, char **av)
 {
-	t_stack stacks;
-
-	ft_init_stacks(&stacks);
+	char	**data;
+//	t_stack	stacks;
+	//ft_init_stacks(&stacks);
+	data = parse_arguments(av, ac);
+	for (int i = 0 ;  data[i] != NULL ; i++)
+	{
+		ft_putstr_fd(data[i],1);
+		ft_putstr_fd("\n",1);
+	}
 	return (0);
 }
--- push_swap.h
+++ push_swap.h
@@ -6,18 +6,17 @@
 /*   By: zjamali <zjamali@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/16 20:28:50 by zjamali           #+#    #+#             */
-/*   Updated: 2021/06/16 20:36:31 by zjamali          ###   ########.fr       */
+/*   Updated: 2021/06/17 14:11:13 by zjamali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 # include "vector_lib/vectors.h"
-
+# include "libft/libft.h"
 typedef struct s_stacks
 {
 	t_vector	a;
 	t_vector	b;
 }				t_stack;
-
 #endif
